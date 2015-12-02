46abd777247a9abea229e4c8a042c5cd525639a9
Austin Eschenbach <austin.eschenbach@gmail.com>
2015-12-01 23:08:59 -0800

    add youtube autoplay

--- js/app.js
+++ js/app.js
@@ -3,17 +3,29 @@ autoPlayYouTubeModal();
 //FUNCTION TO GET AND AUTO PLAY YOUTUBE VIDEO FROM DATATAG
 function autoPlayYouTubeModal() {
     var trigger = $("body").find('[data-toggle="modal"]');
+
     trigger.click(function () {
         var theModal = $(this).data("target"),
             videoSRC = $(this).attr("data-theVideo"),
-            videoSRCauto = videoSRC + "?autoplay=1";
+            videoSRCauto = videoSRC + "?autoplay=1",
+            calcwidth = $(window).innerWidth() * .9 + 'px',
+            calcheight = $(window).innerHeight() * .9 + 'px';
         $(theModal + ' iframe').attr('src', videoSRCauto);
+        $(theModal + ' iframe').attr('height', calcheight);
+        $(theModal + ' iframe').attr('width', calcwidth);
         $(theModal + ' button.close').click(function () {
             $(theModal + ' iframe').attr('src', videoSRC);
         });
         $('.modal').click(function () {
             $(theModal + ' iframe').attr('src', videoSRC);
         });
+    });
+    $(window).resize(function(){
+      var theModal = '#videoModal',
+          calcwidth = $(window).innerWidth() * .9 + 'px',
+          calcheight = $(window).innerHeight() * .9 + 'px';
+      $(theModal + ' iframe').attr('height', calcheight);
+      $(theModal + ' iframe').attr('width', calcwidth);
     });
 }
 $('.contactlink').on('click', function(){
